ac158d79137e2b19266310bb2918c2c1f0d84f9a
vnagepr <prasada.bn2000@gmail.com>
2025-01-09 02:05:18 +0530

    aa

---
+++ ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using CourseAPI.Domains.Models;
+
+namespace CourseAPI.Domains.Interfaces
+{
+    public interface ICourseService
+    {
+        public Task<List<Course>> GetAllCourses();
+        public Task<Course> GetCourseById(string id);
+        public Task<Boolean> SaveCourse(CourseParam course);
+        public Task<Course> UpdateCourse(CourseParam course);
+        public Task<Boolean> DeleteCourseById(string id);
+    }
+}
